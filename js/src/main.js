eeb4a8965ae457a5c1f095969b996a62954cc2e9
John Mick <john.d.mick@gmail.com>
2011-11-21 15:27:43 -0500

    Adjusted default initial y velocity and added IDs for new Slider UI elements

--- js/src/main.js
+++ js/src/main.js
@@ -2,7 +2,6 @@
 	window.Orbits = Orbits({
 		renderer:{
 			earthimg:"./images/earth.png"
-
 		},
 		physics:{
 
@@ -10,14 +9,17 @@
 		ui:{
 			canvas:"CANVAS_BOARD",
 			satmass:"SAT_MASS_INPUT",
+			satmasslbl:"SAT_MASS_LBL",
 			earthmass:"EARTH_MASS_INPUT",
 			earthmasslbl:"EARTH_MASS_LBL",
 			initx:"INIT_X_INPUT",
+			initxlbl:"INIT_X_LBL",
 			inity:"INIT_Y_INPUT",
+			initylbl:"INIT_Y_LBL",
 			defaultsatmass:1,
 			defaultearthmass:1000,
 			defaultinitx: -1,
-			defaultinity: .2
+			defaultinity: -.75 
 		},
 		settings:{
 			earth: {
