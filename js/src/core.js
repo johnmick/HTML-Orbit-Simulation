f24b142136f7a9943b2f0cf26b59c7864c5431c8
John Mick <john.d.mick@gmail.com>
2011-11-21 12:50:08 -0500

    Added Core Function for setting Earth Mass

--- js/src/core.js
+++ js/src/core.js
@@ -2,12 +2,17 @@ var Orbits, Renderer, Physics, UI, Sat;
 var sats = [], earth, debug;
 
 (function(){
-    Orbits = function(opts) {
+	Orbits = function(opts) {
 		earth = opts.settings.earth;
 		Orbits.UI = UI(opts.ui);
 		Orbits.Renderer = Renderer(opts.renderer);
 		Orbits.Physics = Physics(opts.physics);
 		debug = document.getElementById("DEBUG");
 		return Orbits;
-    };
+	};
+
+	Orbits.setEarthMass = function(mass)
+	{
+		earth.m = mass * 10e12;
+	};
 })();
