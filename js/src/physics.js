f113bfab4dbb72f098817b9217944270cec7d7c3
John Mick <john.d.mick@gmail.com>
2011-11-22 09:21:28 -0500

    Fixed sign error in Distance Calculation and using satellite mass for acceleration calculation.
    Also added collision detection for the earth

--- js/src/physics.js
+++ js/src/physics.js
@@ -5,20 +5,33 @@
 
 		Physics.reCalc = function()
 		{
-			for (var sat in sats)
+			var collisions = [];
+			for (var i = sats.length-1; i > -1; i--)
 			{
-				var s = sats[sat];
-				var d = (s.x + earth.x) * (s.x + earth.x) + (s.y + earth.y) * (s.y + earth.y);
-				var theta = Math.atan2(s.y - earth.y, s.x - earth.x);
-				var k = -6.67300e-11 * earth.m * s.m / d;
-				s.u += k * Math.cos(theta);
-				s.v += k * Math.sin(theta);
-				s.xpoints.push(s.x);
-				s.ypoints.push(s.y);
-				s.colors.push(calcLineColor(s.initSpeed, Math.sqrt((s.u * s.u) + (s.v * s.v))));
-				
-				s.x += s.u;
-				s.y += s.v;
+				var s = sats[i];
+				var d = (s.x - earth.x) * (s.x - earth.x) + (s.y - earth.y) * (s.y - earth.y);
+
+				if (d <= earth.r2)
+				{
+					collisions.push(i);
+				}
+				else
+				{
+					var theta = Math.atan2(s.y - earth.y, s.x - earth.x);
+					var k = -6.67300e-11 * earth.m * s.m / d;
+					s.u += (k * Math.cos(theta)) / s.m;
+					s.v += (k * Math.sin(theta)) / s.m;
+					s.xpoints.push(s.x);
+					s.ypoints.push(s.y);
+					s.colors.push(calcLineColor(s.initSpeed, Math.sqrt((s.u * s.u) + (s.v * s.v))));
+					s.x += s.u;
+					s.y += s.v;
+				}
+			}
+
+			for (var i = collisions.length-1; i > -1; i--)
+			{
+				sats.splice(collisions[i], 1);
 			}
 		};
 
