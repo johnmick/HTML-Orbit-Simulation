872518413d72615446c20a319d465873b0efdc94
John Mick <john.d.mick@gmail.com>
2011-11-25 14:06:15 -0500

    Added Minor 'Hack' to Set Earth Mass On Init

--- js/src/core.js
+++ js/src/core.js
@@ -4,6 +4,7 @@ var sats = [], earth, debug;
 (function(){
 	Orbits = function(opts) {
 		earth = opts.settings.earth;
+		Orbits.setEarthMass(earth.m);
 		Orbits.UI = UI(opts.ui);
 		Orbits.Renderer = Renderer(opts.renderer);
 		Orbits.Physics = Physics(opts.physics);
