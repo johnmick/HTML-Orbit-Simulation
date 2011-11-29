14f92ebf5aaca4c93bbd98323f1cd50dcd66ad51
John Mick <john.d.mick@gmail.com>
2011-11-29 12:33:50 -0500

    New Timestep Labels passed to UI - Adjusted Default Parameters and created new initial simulation - Recalcs 31 Times and only stores last Physics Calc

--- js/src/main.js
+++ js/src/main.js
@@ -16,15 +16,18 @@
 			initxlbl:"INIT_X_LBL",
 			inity:"INIT_Y_INPUT",
 			initylbl:"INIT_Y_LBL",
+			timesteplbl:"TIME_STEP_LBL",
+			timestep:"TIME_STEP_INPUT",
 			defaultsatmass:15,
-			defaultearthmass:50,
-			defaultinitx: 1,
-			defaultinity: 1
+			defaultearthmass:100,
+			defaultinitx: 0,
+			defaultinity: 1.5,
+			defaulttimestep:.01
 		},
 		settings:{
 			earth: {
 				x: 310,
-				y: 250,
+				y: 290,
 				r: 10,
 				r2: 10*10,
 				m:50
@@ -34,11 +37,15 @@
 
 	setInterval(function(){
 		Renderer.redraw();
-		Physics.reCalc();
+		for (var i=30; i > -1; i--)
+		{
+			Physics.reCalc();
+		}
+		Physics.reCalc(true);
 	}, 25);
 
-	sats.push(new Sat({x:150, y:150, u:1, v:0, m:15}));
-	sats.push(new Sat({x:150, y:350, u:1, v:0, m:15}));
-	sats.push(new Sat({x:460, y:150, u:-1, v:0, m:15}));
-	sats.push(new Sat({x:460, y:350, u:-1, v:0, m:15}));
+	sats.push(new Sat({x:310, y:180, u:1.5, v:0, m:25, c:"#FF0000"}));
+	sats.push(new Sat({x:310, y:400, u:-1.5, v:0, m:25, c:"#FFFF00"}));
+	sats.push(new Sat({x:200, y:290, u:0, v:1.5, m:25, c:"#FF00FF"}));
+	sats.push(new Sat({x:420, y:290, u:0, v:-1.5, m:25, c:"#00FFFF"}));
 })();
