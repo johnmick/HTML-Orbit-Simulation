7d5d1adc74687370df0dc4d5c53569189ccb5d49
John Mick <John.D.Mick@gmail.com>
2012-02-16 15:40:36 -0500

    Began implementation of N-Body physics, currently had to redefine the Universal Gravitational Constant to notice interactions at the current rendering scale; Also not properly trapping collision events - consider implementing a softening factor to allow objects to pass through one another

--- js/src/physics.js
+++ js/src/physics.js
@@ -1,3 +1,5 @@
+var Physics;
+
 (function(){
 	var timestep = 1;
 
@@ -18,34 +20,59 @@
 	Physics.reCalc = function(store)
 	{
 		var collisions = [];
-		for (var i = sats.length-1; i > -1; i--)
-		{
-			var s = sats[i];
-			var d = (s.x - earth.x) * (s.x - earth.x) + (s.y - earth.y) * (s.y - earth.y);
+		var loop = sats.length-1;
 
-			if (d <= earth.r2)
+		for (var i = loop; i > -1; i--)
+		{
+			var satOne = sats[i];
+			var collided = false;
+
+			for (var k = loop; k > -1; k--)
 			{
-				collisions.push(i);
+				var satTwo = sats[k];
+				if (i !== k)
+				{
+					var xDiff = satOne.x - satTwo.x;
+					var yDiff = satOne.y - satTwo.y;
+					var d = xDiff * xDiff + yDiff * yDiff;
+
+					// Check D for a Collision Event Here Otherwise Proceed
+					// Need to handle collision removal much better
+					if ( d <= satTwo.r2)
+					{
+						collisions.push(i);
+						break;
+					}
+					else
+					{
+						var theta = Math.atan2(yDiff, xDiff);
+						var k = -6.673e-3 * satTwo.m * satOne.m / d;
+						//var k = -6.673e-11 * satTwo.m * satOne.m / d;
+						satOne.u += ((k * Math.cos(theta)) / satOne.m) * timestep;
+						satOne.v += ((k * Math.sin(theta)) / satOne.m) * timestep;
+					}
+				}
 			}
-			else
+
+			if (collided === false)
 			{
-				var theta = Math.atan2(s.y - earth.y, s.x - earth.x);
-				var k = -6.67300e-11 * earth.m * s.m / d;
-				s.u += ((k * Math.cos(theta)) / s.m) * timestep;
-				s.v += ((k * Math.sin(theta)) / s.m) * timestep;
+				satOne.x += satOne.u * timestep;
+				satOne.y += satOne.v * timestep;
+
+				if (satOne.x < 0 || satOne. y < 0 || satOne.x > 1000 || satOne.y > 1000)
+				{
+					// Also not correct
+					collisions.push(i);
+				}
 
 				if (store === true)
 				{
-					s.xpoints.push(s.x);
-					s.ypoints.push(s.y);
-					s.colors.push(calcLineColor(s.initSpeed, Math.sqrt((s.u * s.u) + (s.v * s.v))));
+					satOne.xpoints.push(satOne.x);
+					satOne.ypoints.push(satOne.y);
+					satOne.colors.push(calcLineColor(satOne.initSpeed, Math.sqrt((satOne.u * satOne.u) + (satOne.v * satOne.v))));
 				}
-
-				s.x += s.u * timestep;
-				s.y += s.v * timestep;
 			}
 		}
-
 		for (var i = collisions.length-1; i > -1; i--)
 		{
 			sats.splice(collisions[i], 1);
