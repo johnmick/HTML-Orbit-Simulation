15b41817833c881e833e8f4e18b09c6554942304
John Mick <John.D.Mick@gmail.com>
2012-02-16 15:39:36 -0500

    Removed Earth References, Added Information for Radius UI, Created a Testing Timer that terminates after a configurable number of iterations, created new initial satellites to begin experimentation with n-body behavior

--- js/src/main.js
+++ js/src/main.js
@@ -1,51 +1,54 @@
 (function(){
 	window.Orbits = Orbits({
 		renderer:{
-			earthimg:"./images/earth.png"
+
 		},
 		physics:{
 
 		},
 		ui:{
 			canvas:"CANVAS_BOARD",
+			satradius:"SAT_RADIUS_INPUT",
+			satradiuslbl:"SAT_RADIUS_LBL",
 			satmass:"SAT_MASS_INPUT",
 			satmasslbl:"SAT_MASS_LBL",
-			earthmass:"EARTH_MASS_INPUT",
-			earthmasslbl:"EARTH_MASS_LBL",
 			initx:"INIT_X_INPUT",
 			initxlbl:"INIT_X_LBL",
 			inity:"INIT_Y_INPUT",
 			initylbl:"INIT_Y_LBL",
 			timesteplbl:"TIME_STEP_LBL",
 			timestep:"TIME_STEP_INPUT",
+			defaultsatradius:5,
 			defaultsatmass:15,
-			defaultearthmass:100,
 			defaultinitx: 0,
 			defaultinity: 1.5,
 			defaulttimestep:.01
 		},
 		settings:{
-			earth: {
-				x: 310,
-				y: 290,
-				r: 10,
-				r2: 10*10,
-				m:50
-			}
+
 		}
 	});
 
-	setInterval(function(){
+	UI.addSatellite({x:300+25, y:200-25, u:0, v:0, m:25, r:5, c:"#FF0000"});
+	UI.addSatellite({x:375, y:275, u:0, v:0, m:25, r:5, c:"#FF0000"});
+	UI.addSatellite({x:300, y:350, u:0, v:0, m:25, r:5, c:"#FFFF00"});
+	UI.addSatellite({x:225-25, y:275+25, u:0, v:0, m:25, r:5, c:"#FFFF00"});
+
+	var times = 100000;
+	var start = 0;
+	var timer = setInterval(function(){
 		Renderer.redraw();
 		for (var i=30; i > -1; i--)
 		{
 			Physics.reCalc();
 		}
 		Physics.reCalc(true);
+		start++;
+		if (times === start)
+		{
+			console.log("Done");
+			clearInterval(timer);
+		}
 	}, 25);
 
-	sats.push(new Sat({x:310, y:180, u:1.5, v:0, m:25, c:"#FF0000"}));
-	sats.push(new Sat({x:310, y:400, u:-1.5, v:0, m:25, c:"#FFFF00"}));
-	sats.push(new Sat({x:200, y:290, u:0, v:1.5, m:25, c:"#FF00FF"}));
-	sats.push(new Sat({x:420, y:290, u:0, v:-1.5, m:25, c:"#00FFFF"}));
 })();
