c02ccdaca9beca23dc7bf9aaa1735315426234da
John Mick <John.D.Mick@gmail.com>
2012-02-16 15:38:41 -0500

    Removed references to an Earth object

--- js/src/core.js
+++ js/src/core.js
@@ -1,19 +1,11 @@
-var Orbits, Renderer, Physics, UI, Sat;
-var sats = [], earth, debug;
+var Orbits, sats=[], debug;
 
 (function(){
 	Orbits = function(opts) {
-		earth = opts.settings.earth;
-		Orbits.setEarthMass(earth.m);
 		Orbits.UI = UI(opts.ui);
 		Orbits.Renderer = Renderer(opts.renderer);
 		Orbits.Physics = Physics(opts.physics);
 		debug = document.getElementById("DEBUG");
 		return Orbits;
-	};
-
-	Orbits.setEarthMass = function(mass)
-	{
-		earth.m = mass * 10e10;
 	};
 })();
